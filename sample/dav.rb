b9e4f27cf2d67cc157f7ed0f0034a4b7df92796e
nahi <nahi@a74cc93d-ecea-0310-97c0-aac310535a50>
2008-12-08 14:29:18 +0000

    * added propfind sample.  see #184.

--- sample/dav.rb
+++ sample/dav.rb
@@ -1,5 +1,5 @@
 require 'uri'
-require 'httpclient'
+require 'http-access2'
 
 class DAV
   attr_reader :headers
@@ -10,10 +10,6 @@ class DAV
     open(uri) if uri
     proxy = ENV['HTTP_PROXY'] || ENV['http_proxy'] || nil
     @client = HTTPClient.new(proxy)
-  end
-
-  def out
-    STDOUT
   end
 
   def open(uri)
@@ -28,6 +24,14 @@ class DAV
     @client.set_basic_auth(@uri, user_id, passwd)
   end
 
+  # TODO: propget/propset support
+
+  def propfind(target)
+    target_uri = @uri + target
+    res = @client.propfind(target_uri)
+    res.body.content
+  end
+
   def get(target, local = nil)
     local ||= target
     target_uri = @uri + target
@@ -39,14 +43,61 @@ class DAV
       f << data
     end
     f.close
-    out.puts("#{ res.header['content-length'][0] } bytes saved to file #{ target }.")
+    STDOUT.puts("#{ res.header['content-length'][0] } bytes saved to file #{ target }.")
   end
 
-  def put(local, target = nil)
-    target ||= local
+  def debug_dev=(dev)
+    @client.debug_dev = dev
+  end
+
+  def get_content(target)
     target_uri = @uri + target
-    out.puts("Sending file #{ local }.")
-    res = @client.put(target_uri, File.open(local, "rb"), @headers)
-    out.puts res.content.read
+    @client.get_content(target_uri, nil, @headers)
+  end
+
+  def put_content(target, content)
+    target_uri = @uri + target
+    res = @client.put(target_uri, content, @headers)
+    if res.status < 200 or res.status >= 300
+      raise "HTTP PUT failed: #{res.inspect}"
+    end
+  end
+
+  class Mock
+    attr_reader :headers
+
+    def initialize(uri = nil)
+      @uri = nil
+      @headers = {}
+      open(uri) if uri
+
+      @cache = {}
+    end
+
+    def open(uri)
+      @uri = uri.is_a?(URI) ?  uri : URI.parse(uri)
+    end
+
+    def set_basic_auth(user_id, passwd)
+      # ignore
+    end
+
+    def propfind(target)
+      # not found
+      nil
+    end
+
+    def get(target, local = nil)
+      # ignore
+    end
+
+    def get_content(target)
+      @cache[target]
+    end
+
+    def put_content(target, content)
+      @cache[target] = content
+      nil
+    end
   end
 end
