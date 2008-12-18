c4f058af3a552ecf5df926ffc1e2b92623f258b7
nahi <nahi@a74cc93d-ecea-0310-97c0-aac310535a50>
2008-12-18 14:46:53 +0000

    * added try_require which ignores LoadError.

--- bench/bm.rb
+++ bench/bm.rb
@@ -1,12 +1,21 @@
-require 'httpclient'
-require 'rfuzz/session'
-require 'net/http'
 require 'benchmark'
 require 'uri'
+
+require 'httpclient'
+require 'net/http'
 require 'open-uri'
-require 'eventmachine'
-require 'curb'
-require 'httparty'
+
+def try_require(target)
+  begin
+    require target
+  rescue LoadError
+  end
+end
+
+try_require 'rfuzz/session'
+try_require 'eventmachine'
+try_require 'curb'
+try_require 'httparty'
 
 url = ARGV.shift or raise
 proxy = ENV['http_proxy'] || ENV['HTTP_PROXY']
@@ -58,77 +67,89 @@ Benchmark.bmbm do |bm|
   end
 =end
 
-  bm.report('curb') do
-    do_threads(threads) {
-      (1..number).collect {
-        Curl::Easy.http_get(url.to_s).body_str.size
-      }
-    }
-  end
-
-  bm.report('RFuzz::HttpClient') do
-    do_threads(threads) {
-      if proxy
-        host, port = proxy.host, proxy.port
-      else
-        host, port = url.host, url.port
-      end
-      path = proxy ? url.to_s : url.path
-      c = RFuzz::HttpClient.new(host, port)
-      result = (1..number).collect {
-	c.get(path).http_body.size
-      }
-      c.reset
-      result
-    }
-  end
-
-  bm.report('Net::HTTP') do
-    do_threads(threads) {
-      if proxy
-        c = Net::HTTP::Proxy(proxy.host, proxy.port).new(url.host, url.port)
-      else
-        c = Net::HTTP.new(url.host, url.port)
-      end
-      c.start
-      result = (1..number).collect {
-	c.get(url.path).read_body.size
-      }
-      c.finish
-      result
-    }
-  end
-
-  bm.report('HTTPClient') do
-    c = HTTPClient.new(proxy)
-    do_threads(threads) {
-      (1..number).collect {
-	c.get_content(url).size
-      }
-    }
-    c.reset_all
-  end
-
-  bm.report('open-uri') do
-    do_threads(threads) {
-      (1..number).collect {
-        open(url, :proxy => proxy) { |f|
-          f.read.size
+  if defined?(Curl)
+    bm.report('curb') do
+      do_threads(threads) {
+        (1..number).collect {
+          Curl::Easy.http_get(url.to_s).body_str.size
         }
       }
-    }
+    end
   end
 
-  class HTTPartyClient # need to create subclass for http_proxy
-    include HTTParty
-  end
-  bm.report('HTTParty') do
-    HTTPartyClient.http_proxy(proxy.host, proxy.port) if proxy
-    do_threads(threads) {
-      (1..number).collect {
-        # HTTParty should accept URI object like others.
-        HTTPartyClient.get(url.to_s).size
+  if defined?(RFuzz)
+    bm.report('RFuzz::HttpClient') do
+      do_threads(threads) {
+        if proxy
+          host, port = proxy.host, proxy.port
+        else
+          host, port = url.host, url.port
+        end
+        path = proxy ? url.to_s : url.path
+        c = RFuzz::HttpClient.new(host, port)
+        result = (1..number).collect {
+          c.get(path).http_body.size
+        }
+        c.reset
+        result
       }
-    }
+    end
+  end
+
+  if defined?(Net::HTTP)
+    bm.report('Net::HTTP') do
+      do_threads(threads) {
+        if proxy
+          c = Net::HTTP::Proxy(proxy.host, proxy.port).new(url.host, url.port)
+        else
+          c = Net::HTTP.new(url.host, url.port)
+        end
+        c.start
+        result = (1..number).collect {
+          c.get(url.path).read_body.size
+        }
+        c.finish
+        result
+      }
+    end
+  end
+
+  if defined?(HTTPClient)
+    bm.report('HTTPClient') do
+      c = HTTPClient.new(proxy)
+      do_threads(threads) {
+        (1..number).collect {
+          c.get_content(url).size
+        }
+      }
+      c.reset_all
+    end
+  end
+
+  if defined?(OpenURI)
+    bm.report('open-uri') do
+      do_threads(threads) {
+        (1..number).collect {
+          open(url, :proxy => proxy) { |f|
+            f.read.size
+          }
+        }
+      }
+    end
+  end
+
+  if defined?(HTTParty)
+    class HTTPartyClient # need to create subclass for http_proxy
+      include HTTParty
+    end
+    bm.report('HTTParty') do
+      HTTPartyClient.http_proxy(proxy.host, proxy.port) if proxy
+      do_threads(threads) {
+        (1..number).collect {
+          # HTTParty should accept URI object like others.
+          HTTPartyClient.get(url.to_s).size
+        }
+      }
+    end
   end
 end
--- bench/prof.rb
+++ bench/prof.rb
@@ -1,15 +1,15 @@
 require 'httpclient'
 require 'ruby-prof'
 
-num = 100
+num = 1
 url = ARGV.shift
 url = URI.parse(url)
 c = HTTPClient.new
 c.debug_dev = STDOUT if $DEBUG
 GC.disable
 RubyProf.start
-num.times do
-  c.get(url).content.size
+File.open('testfile_18826') do |file|
+  puts c.post(url, {'upload' => file}).content
 end
 result = RubyProf.stop
 printer = RubyProf::GraphHtmlPrinter.new(result)
