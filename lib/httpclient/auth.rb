nahi/httpclient

commit e3da6870f5468c744bfc9804156d2cd83d1064bd

Checks only authenticators that are set
We want the ability to only set certain kinds of authenticators if we
already know what they will accept.

Explanation:
1) We know that one of our remote systems *anly* accepts BasicAuth
2) We have limited, expensive bandwidth and our messages always contain a
large attachment
3) If we use the standard HttpClient, which goes through the whole gamut of
authenticators... it will continually send this large attachment multiple
times... using authenticators that we *already know* will be refused, thus
wasting our bandwidth and time.

Therefore, we want to restrict the authenticator set to only the known
authenticators... when we *already know* the authentication schemes that the
remote system accepts.

The way I'e done this is quite simple - and the ddefault behaviour of the
system will remain the same.

When you "set" the authenticator, it marks it as usable.
If you use the default "set_auth" method - it will mark all authenticators
as usable, just as it curently does.

*but* if you already know the authentication method... then only set the
methods you will use...

For example, if you know that it will only accept basic auth, and negotiate
auth, use:

@client = HTTPClient::WWWauth.new
@client.basic_auth.set_auth('myuser','mypass')
@client.negotiate_auth.set_auth('myuser','mypass')

More-complicated authenticators may require other params, or even no params
eg the following doesn't actually *need* the given params...
@client.sspi_negotiate_auth.set_auth('uri.com','myuser','mypass')

This change frees us up to use the nice fallback negotiation if we need it,
but does not force us to go through the waterfall process if it's *not*
needed.

Having options is a Good Thing.

lib/httpclient/auth.rb
@@ -103,6 +103,7 @@ class HTTPClient
     # 'Authorization' header if needed.
     def filter_request(req)
       @authenticator.each do |auth|
+        next unless auth.set? # hasn't be set, don't use it
         if cred = auth.get(req)
           req.header.set('Authorization', auth.scheme + " " + cred)
           return
@@ -119,6 +120,7 @@ class HTTPClient
           uri = req.header.request_uri
           challenge.each do |scheme, param_str|
             @authenticator.each do |auth|
+              next unless auth.set? # hasn't be set, don't use it
               if scheme.downcase == auth.scheme.downcase
                 challengeable = auth.challenge(uri, param_str)
                 command = :retry if challengeable
@@ -179,6 +181,7 @@ class HTTPClient
     # 'Proxy-Authorization' header if needed.
     def filter_request(req)
       @authenticator.each do |auth|
+        next unless auth.set? # hasn't be set, don't use it
         if cred = auth.get(req)
           req.header.set('Proxy-Authorization', auth.scheme + " " + cred)
           return
@@ -195,6 +198,7 @@ class HTTPClient
           uri = req.header.request_uri
           challenge.each do |scheme, param_str|
             @authenticator.each do |auth|
+              next unless auth.set? # hasn't be set, don't use it
               if scheme.downcase == auth.scheme.downcase
                 challengeable = auth.challenge(uri, param_str)
                 command = :retry if challengeable
@@ -219,6 +223,7 @@ class HTTPClient
     # Creates new BasicAuth filter.
     def initialize
       @cred = nil
+      @set = false
       @auth = {}
       @challengeable = {}
       @scheme = "Basic"
@@ -233,6 +238,7 @@ class HTTPClient
     # Set authentication credential.
     # uri == nil for generic purpose (allow to use user/password for any URL).
     def set(uri, user, passwd)
+      @set = true
       if uri.nil?
         @cred = ["#{user}:#{passwd}"].pack('m').tr("\n", '')
       else
@@ -240,6 +246,12 @@ class HTTPClient
         @auth[uri] = ["#{user}:#{passwd}"].pack('m').tr("\n", '')
       end
     end
+
+    # have we marked this as set - ie that it's valid to use in this context?
+    def set?
+      @set == true
+    end
+
 
     # Response handler: returns credential.
     # It sends cred only when a given uri is;
@@ -274,6 +286,7 @@ class HTTPClient
     def initialize
       @auth = {}
       @challenge = {}
+      @set = false
       @nonce_count = 0
       @scheme = "Digest"
     end
@@ -287,10 +300,16 @@ class HTTPClient
     # Set authentication credential.
     # uri == nil is ignored.
     def set(uri, user, passwd)
+      @set = true
       if uri
         uri = Util.uri_dirname(uri)
         @auth[uri] = [user, passwd]
       end
+    end
+
+    # have we marked this as set - ie that it's valid to use in this context?
+    def set?
+      @set == true
     end
 
     # Response handler: returns credential.
@@ -395,6 +414,7 @@ class HTTPClient
       @auth_default = nil
       @challenge = {}
       @scheme = scheme
+      @set = false
       @ntlm_opt = {
         :ntlmv2 => true
       }
@@ -409,12 +429,18 @@ class HTTPClient
     # Set authentication credential.
     # uri == nil for generic purpose (allow to use user/password for any URL).
     def set(uri, user, passwd)
+      @set = true
       if uri
         uri = Util.uri_dirname(uri)
         @auth[uri] = [user, passwd]
       else
         @auth_default = [user, passwd]
       end
+    end
+
+    # have we marked this as set - ie that it's valid to use in this context?
+    def set?
+      @set == true
     end
 
     # Response handler: returns credential.
@@ -476,6 +502,7 @@ class HTTPClient
     # Creates new SSPINegotiateAuth filter.
     def initialize
       @challenge = {}
+      @set = false
       @scheme = "Negotiate"
     end
 
@@ -489,7 +516,13 @@ class HTTPClient
     # NOT SUPPORTED: username and necessary data is retrieved by win32/sspi.
     # See win32/sspi for more details.
     def set(uri, user, passwd)
+      @set = true
       # not supported
+    end
+
+    # have we marked this as set - ie that it's valid to use in this context?
+    def set?
+      @set == true
     end
 
     # Response handler: returns credential.
@@ -625,6 +658,7 @@ class HTTPClient
         'HMAC-SHA1' => method(:sign_hmac_sha1)
       }
       @scheme = "OAuth"
+      @set = false
     end
 
     # Resets challenge state.  Do not send '*Authorization' header until the
@@ -636,7 +670,13 @@ class HTTPClient
     # Set authentication credential.
     # You cannot set OAuth config via WWWAuth#set_auth. Use OAuth#config=
     def set(uri, user, passwd)
+      @set = true
       # not supported
+    end
+
+    # have we marked this as set - ie that it's valid to use in this context?
+    def set?
+      @set == true
     end
 
     # Set authentication credential.

commit 7820bec1f74ce13831df798705f266322cdee25d

Recover authentication features for OAuth and Negotiate.
Auth#set? introduced at e3da6870f5468c744bfc broke OAuth and Negotiate.
These authenticators must be checked against URI given so we cannot skip
these in response_filter.

lib/httpclient/auth.rb
@@ -259,7 +259,6 @@ class HTTPClient
       @set == true
     end
 
-
     # Response handler: returns credential.
     # It sends cred only when a given uri is;
     # * child page of challengeable(got *Authenticate before) uri and,
@@ -509,7 +508,6 @@ class HTTPClient
     # Creates new SSPINegotiateAuth filter.
     def initialize
       @challenge = {}
-      @set = false
       @scheme = "Negotiate"
     end
 
@@ -523,13 +521,12 @@ class HTTPClient
     # NOT SUPPORTED: username and necessary data is retrieved by win32/sspi.
     # See win32/sspi for more details.
     def set(*args)
-      @set = true
       # not supported
     end
 
     # have we marked this as set - ie that it's valid to use in this context?
     def set?
-      @set == true
+      SSPIEnabled || GSSAPIEnabled
     end
 
     # Response handler: returns credential.
@@ -546,7 +543,7 @@ class HTTPClient
       authphrase = param[:authphrase]
       case state
       when :init
-        if(SSPIEnabled)
+        if SSPIEnabled
           authenticator = param[:authenticator] = Win32::SSPI::NegotiateAuth.new
           return authenticator.get_initial_token(@scheme)
         else # use GSSAPI
@@ -556,7 +553,7 @@ class HTTPClient
         end
       when :response
         @challenge.delete(domain_uri)
-        if(SSPIEnabled)
+        if SSPIEnabled
           return authenticator.complete_authentication(authphrase)
         else # use GSSAPI
           return authenticator.init_context(authphrase.unpack('m').pop)
@@ -675,7 +672,6 @@ class HTTPClient
         'HMAC-SHA1' => method(:sign_hmac_sha1)
       }
       @scheme = "OAuth"
-      @set = false
     end
 
     # Resets challenge state.  Do not send '*Authorization' header until the
@@ -687,13 +683,12 @@ class HTTPClient
     # Set authentication credential.
     # You cannot set OAuth config via WWWAuth#set_auth. Use OAuth#config=
     def set(*args)
-      @set = true
       # not supported
     end
 
     # have we marked this as set - ie that it's valid to use in this context?
     def set?
-      @set == true
+      true
     end
 
     # Set authentication credential.