nahi/httpclient

commit f29310d2726b1723451af293c8366f820b2ce33c

Support empty (but set) http_proxy ENV variable
Just treat it the same as if it was nil/unset

lib/httpclient.rb
@@ -438,7 +438,7 @@ class HTTPClient
   #
   # Calling this method resets all existing sessions.
   def proxy=(proxy)
-    if proxy.nil?
+    if proxy.nil? || proxy.empty?
       @proxy = nil
       @proxy_auth.reset_challenge
     else

test/test_httpclient.rb
@@ -234,6 +234,15 @@ class TestHTTPClient < Test::Unit::TestCase
     end
   end
 
+  def test_empty_proxy_env
+    setup_proxyserver
+    escape_env do
+      ENV['http_proxy'] = ""
+      client = HTTPClient.new
+      assert_equal(nil, client.proxy)
+    end
+  end
+
   def test_noproxy_for_localhost
     @proxyio.string = ""
     @client.proxy = @proxyurl

commit 796a731ac7da4cbac779c9c8f7bfc73a25289c69

Regression: setting proxy with URI
client.proxy = '' and client.proxy = URI.parse('') broken by
f29310d2726b1723451af293c8366f820b2ce33c. Fixed.

lib/httpclient.rb
@@ -438,7 +438,7 @@ class HTTPClient
   #
   # Calling this method resets all existing sessions.
   def proxy=(proxy)
-    if proxy.nil? || proxy.empty?
+    if proxy.nil? || proxy.to_s.empty?
       @proxy = nil
       @proxy_auth.reset_challenge
     else

test/test_http-access2.rb
@@ -117,9 +117,8 @@ class TestClient < Test::Unit::TestCase
       assert_raises(URI::InvalidURIError) do
        	@client.proxy = "http://"
       end
-      assert_raises(ArgumentError) do
-	@client.proxy = ""
-      end
+      @client.proxy = ""
+      assert_nil(@client.proxy)
       @client.proxy = "http://foo:1234"
       assert_equal(URI.parse("http://foo:1234"), @client.proxy)
       uri = URI.parse("http://bar:2345")

test/test_httpclient.rb
@@ -153,9 +153,8 @@ class TestHTTPClient < Test::Unit::TestCase
       assert_raises(URI::InvalidURIError) do
        	@client.proxy = "http://"
       end
-      assert_raises(ArgumentError) do
-	@client.proxy = ""
-      end
+      @client.proxy = ""
+      assert_nil(@client.proxy)
       @client.proxy = "http://admin:admin@foo:1234"
       assert_equal(URI.parse("http://admin:admin@foo:1234"), @client.proxy)
       uri = URI.parse("http://bar:2345")