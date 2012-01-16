8ed5b25768689f6120c57263362242b0a35aea66
Emanuel Alvarez <ema@abductedcow.com.ar>
2012-01-16 20:07:41 -0300

    add support for keep alive proxy connections

--- lib/httpclient/session.rb
+++ lib/httpclient/session.rb
@@ -729,12 +729,12 @@ class HTTPClient
       retry_number = 0
       begin
         timeout(@connect_timeout, ConnectTimeoutError) do
-          @socket = create_socket(site)
+          @socket = create_socket(site) unless @next_connection
           if https?(@dest)
             if @socket.is_a?(LoopBackSocket)
               connect_ssl_proxy(@socket, URI.parse(@dest.to_s)) if @proxy
             else
-              @socket = create_ssl_socket(@socket)
+              @socket = create_ssl_socket(@socket) unless @next_connection
               connect_ssl_proxy(@socket, URI.parse(@dest.to_s)) if @proxy
               @socket.ssl_connect(@dest.host)
               @socket.post_connection_check(@dest)
@@ -749,10 +749,12 @@ class HTTPClient
       rescue RetryableResponse
         retry_number += 1
         if retry_number < @protocol_retry_count
+          consume_body if @next_connection
           retry
         end
         raise BadResponseError.new("connect to the server failed with status #{@status} #{@reason}")
-      rescue TimeoutError
+      rescue TimeoutError, KeepAliveDisconnected
+        @next_connection = nil
         if @connect_retry == 0
           retry
         else
@@ -802,6 +804,8 @@ class HTTPClient
         filter.filter_request(req)
       end
       set_header(req)
+      # TODO: this header should be optional
+      req.header.set('Connection', 'keep-alive')
       req.dump(@socket)
       @socket.flush unless @socket_sync
       res = HTTP::Message.new_response('')
@@ -984,6 +988,12 @@ class HTTPClient
           return
         end
       end
+    end
+
+    def consume_body
+      @state = :DATA
+      get_body { }
+      @state = :INIT
     end
   end
 
