6899c423328b16bbf664e2f99eb6ddfca2f9d985
Florian Hars <hars@pre-sense.de>
2012-06-01 17:09:16 +0200

    test digest proxy auth with a real proxy

--- test/test_auth.rb
+++ test/test_auth.rb
@@ -56,6 +56,17 @@ class TestAuth < Test::Unit::TestCase
       :UserDB => htdigest_userdb
     )
     @server_thread = start_server_thread(@server)
+
+
+    @proxyserver = WEBrick::HTTPProxyServer.new(
+      :ProxyAuthProc => @digest_auth,
+      :BindAddress => "localhost",
+      :Logger => @proxylogger,
+      :Port => 0,
+      :AccessLog => []
+    )
+    @proxyport = @proxyserver.config[:Port]
+    @proxyserver_thread = start_server_thread(@proxyserver)
   end
 
   def do_basic_auth(req, res)
@@ -205,7 +216,7 @@ class TestAuth < Test::Unit::TestCase
     assert_match(/Proxy-Authorization: Basic YWRtaW46YWRtaW4=/, str)
   end
 
-  def test_digest_proxy_auth
+  def test_digest_proxy_auth_loop
     c = HTTPClient.new
     c.set_proxy_auth('admin', 'admin')
     c.test_loopback_http_response << "HTTP/1.0 407 Unauthorized\nProxy-Authenticate: Digest realm=\"foo\", nonce=\"nonce\", stale=false\nContent-Length: 2\n\nNG"
@@ -218,6 +229,13 @@ class TestAuth < Test::Unit::TestCase
     c.get_content('http://example.com/')
     assert_match(/Proxy-Authorization: Digest/, str)
     assert_match(%r"response=\"#{response}\"", str)
+  end
+
+  def test_digest_proxy_auth
+    c=HTTPClient.new("http://localhost:#{proxyport}/")
+    c.set_proxy_auth('admin', 'admin')
+    c.set_auth("http://localhost:#{serverport}/", 'admin', 'admin')
+    assert_equal('basic_auth OK', c.get_content("http://localhost:#{serverport}/basic_auth"))
   end
 
   def test_prefer_digest_to_basic_proxy_auth
