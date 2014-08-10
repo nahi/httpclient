b3077686ee2f07f7ed6d2a997555202940667e1e
glebtv <glebtv@gmail.com>
2014-08-10 14:03:58 +0400

    thread safe DNS cache

--- lib/httpclient/lru_cache.rb
+++ lib/httpclient/lru_cache.rb
@@ -4,25 +4,53 @@
 # (MIT Licensed). Thanks!
 
 require "lru_redux"
+require 'thread'
 
-# Not thread-safe!
 class HTTPClient
   class LRUCache
 
     attr_reader :max_size, :ttl, :soft_ttl, :retry_delay
 
     def initialize(opts={})
+      @mutex = Mutex.new
+
       @max_size = Integer(opts[:max_size] || 100)
       @ttl = Float(opts[:ttl] || 0)
       @soft_ttl = Float(opts[:soft_ttl] || 0)
       @retry_delay = Float(opts[:retry_delay] || 0)
+
       raise "max_size must not be negative" if @max_size < 0
       raise "ttl must not be negative" if @ttl < 0
       raise "soft_ttl must not be negative" if @soft_ttl < 0
       raise "retry_delay must not be negative" if @retry_delay < 0
 
+      # we don't use thread safe variant, as we're doing thread safety ourselves
       @data = LruRedux::Cache.new(@max_size)
     end
+
+    def fetch(key)
+      # for now, this means that DNS resolving is single-threaded
+      @mutex.synchronize do
+        datum = @data[key]
+        if datum.nil?
+          store(key, value = yield)
+        elsif datum.expired?
+          @data.delete(key)
+          store(key, value = yield)
+        elsif datum.soft_expired?
+          begin
+            store(key, value = yield)
+          rescue RuntimeError => e
+            datum.soft_expiration = (Time.now + retry_delay) if retry_delay > 0
+            datum.value
+          end
+        else
+          datum.value
+        end
+      end
+    end
+
+    private
 
     def store(key, value)
       expiration = Time.now + @ttl
@@ -30,31 +58,6 @@ class HTTPClient
       @data[key] = Datum.new(value, expiration, soft_expiration)
       value
     end
-
-    alias :[]= :store
-
-    def fetch(key)
-      datum = @data[key]
-      if datum.nil?
-        store(key, value = yield)
-      elsif datum.expired?
-        @data.delete(key)
-        store(key, value = yield)
-      elsif datum.soft_expired?
-        begin
-          store(key, value = yield)
-        rescue RuntimeError => e
-          datum.soft_expiration = (Time.now + retry_delay) if retry_delay > 0
-          datum.value
-        end
-      else
-        datum.value
-      end
-    end
-
-    alias :[] :fetch
-
-    private
 
     class Datum
       attr_reader :value, :expiration, :soft_expiration
