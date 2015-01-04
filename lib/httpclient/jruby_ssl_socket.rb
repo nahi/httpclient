558f0f75bdc0560bb8dc3a0723fe096f0746ef28
Hiroshi Nakamura <nahi@ruby-lang.org>
2015-01-04 14:49:03 +0900

    Implement verify_mode, callback and ssl_version

--- lib/httpclient/jruby_ssl_socket.rb
+++ lib/httpclient/jruby_ssl_socket.rb
@@ -15,21 +15,193 @@ class HTTPClient
 unless defined?(SSLSocket)
 
   class JRubySSLSocket
-    java_import 'javax.net.ssl.SSLSocketFactory'
     java_import 'java.io.BufferedInputStream'
+    java_import 'java.security.KeyStore'
+    java_import 'javax.net.ssl.SSLContext'
+    java_import 'javax.net.ssl.SSLSocketFactory'
+    java_import 'javax.net.ssl.TrustManager'
+    java_import 'javax.net.ssl.TrustManagerFactory'
+    java_import 'javax.net.ssl.X509TrustManager'
+
+    class JavaCertificate
+      def initialize(cert)
+        @cert = cert
+      end
+
+      def subject
+        @cert.getSubjectDN
+      end
+
+      def to_text
+        @cert.toString
+      end
+
+      def to_pem
+        '(not in PEM format)'
+      end
+    end
+
+    class SSLStoreContext
+      attr_reader :current_cert, :chain, :error_depth, :error, :error_string
+
+      def initialize(current_cert, chain, error_depth, error, error_string)
+        @current_cert, @chain, @error_depth, @error, @error_string =
+          current_cert, chain, error_depth, error, error_string
+      end
+    end
+
+    class JSSEVerifyCallback
+      def initialize(verify_callback)
+        @verify_callback = verify_callback
+      end
+
+      def call(is_ok, chain, error_depth = -1, error = -1, error_string = '(unknown)')
+        if @verify_callback
+          ruby_chain = chain.map { |cert|
+            JavaCertificate.new(cert)
+          }.reverse
+          # NOTE: The order depends on provider implementation
+          ruby_chain.each do |cert|
+            is_ok = @verify_callback.call(
+              is_ok,
+              SSLStoreContext.new(cert, ruby_chain, error_depth, error, error_string)
+            )
+          end
+        end
+        is_ok
+      end
+    end
+
+    class VerifyNoneTrustManagerFactory
+      class VerifyNoneTrustManager
+        include X509TrustManager
+
+        def initialize(verify_callback)
+          @verify_callback = JSSEVerifyCallback.new(verify_callback)
+        end
+
+        def checkServerTrusted(chain, authType)
+          @verify_callback.call(true, chain)
+        end
+
+        def checkClientTrusted(chain, authType); end
+        def getAcceptedIssuers; end
+      end
+
+      def initialize(verify_callback = nil)
+        @verify_callback = verify_callback
+      end
+
+      def init(trustStore)
+        @managers = [VerifyNoneTrustManager.new(@verify_callback)].to_java(X509TrustManager)
+      end
+
+      def getTrustManagers
+        @managers
+      end
+    end
+
+    class SystemTrustManagerFactory
+      class SystemTrustManager
+        include X509TrustManager
+
+        def initialize(original, verify_callback)
+          @original = original
+          @verify_callback = JSSEVerifyCallback.new(verify_callback)
+        end
+
+        def checkServerTrusted(chain, authType)
+          is_ok = false
+          excn = nil
+          # TODO can we detect the depth from excn?
+          error_depth = -1
+          error = nil
+          error_message = nil
+          begin
+            @original.checkServerTrusted(chain, authType)
+            is_ok = true
+          rescue java.security.cert.CertificateException => excn
+            is_ok = false
+            error = excn.class.name
+            error_message = excn.getMessage
+          end
+          is_ok = @verify_callback.call(is_ok, chain, error_depth, error, error_message)
+          unless is_ok
+            excn ||= RuntimeError.new('verifycallback failed')
+            raise excn
+          end
+        end
+
+        def checkClientTrusted(chain, authType); end
+        def getAcceptedIssuers; end
+      end
+
+      def initialize(verify_callback = nil)
+        tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm)
+        tmf.java_method(:init, [KeyStore]).call(nil)
+        @original = tmf.getTrustManagers.find { |tm|
+          tm.is_a?(X509TrustManager)
+        }
+        @verify_callback = verify_callback
+      end
+
+      def init(trustStore)
+        # TODO: trustStore?
+        @managers = [SystemTrustManager.new(@original, @verify_callback)].to_java(X509TrustManager)
+      end
+
+      def getTrustManagers
+        @managers
+      end
+    end
 
     def self.create_socket(session)
       # TODO proxy
+      # TODO post_connection_check
+      #
+      # TODO cert_store (set_default_paths, add_trust_ca_to_store) -> trustStore(certs)
+      # TODO client_cert/client_key -> keyStore(key, cert)
+
+      # TODO OpenSSL specific options are ignored;
+      # ssl_config.verify_depth
+      # ssl_config.options
+      # ssl_config.ciphers
+      # ssl_config.client_ca
+
+      # TODO revocation is performed by -Dcom.sun.security.enableCRLDP=true -Dcom.sun.net.ssl.checkRevocation=true
+      # example: https://test-sspev.verisign.com:2443/test-SSPEV-revoked-verisign.html
       new(session.dest, session.ssl_config, session.debug_dev)
     end
 
-    def initialize(dest, context, debug_dev = nil)
+    def initialize(dest, config, debug_dev = nil)
       # TODO context: the hard part...
       @debug_dev = debug_dev
-      factory = SSLSocketFactory.getDefault
+
+      if config.ssl_version == :auto
+        ssl_version = 'TLS'
+      else
+        ssl_version = config.to_s.gsub(/_/, '.')
+      end
+      if config.verify_mode == nil
+        tmf = VerifyNoneTrustManagerFactory.new(config.verify_callback)
+      else
+        tmf = SystemTrustManagerFactory.new(config.verify_callback)
+      end
+      tmf.init(nil)
+
+      # TODO: load trustStore
+      ctx = SSLContext.getInstance(ssl_version)
+      ctx.init(nil, tmf.getTrustManagers, nil)
+      if config.timeout
+        ctx.getClientSessionContext.setSessionTimeout(config.timeout)
+      end
+
+      # factory = SSLSocketFactory.getDefault
+      factory = ctx.getSocketFactory
       begin
         @ssl_socket = factory.createSocket(dest.host, dest.port)
         @ssl_socket.startHandshake
+        @peer_cert = JavaCertificate.new(@ssl_socket.getSession.getPeerCertificateChain.first)
         @outstr = @ssl_socket.getOutputStream
         @instr = BufferedInputStream.new(@ssl_socket.getInputStream)
         @buf = (' ' * (1024 * 16)).to_java_bytes
@@ -42,7 +214,7 @@ unless defined?(SSLSocket)
     end
 
     def peer_cert
-      # TODO
+      @peer_cert
     end
 
     def close
