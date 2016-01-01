f44fe7f7be5121c4a007bec1f4447713edcd6f1c
Hiroshi Nakamura <nahi@ruby-lang.org>
2016-01-01 23:06:32 +0900

    sleep 1 to prevent too quick retry

--- test/test_auth.rb
+++ test/test_auth.rb
@@ -465,6 +465,7 @@ class TestAuth < Test::Unit::TestCase
       # TODO: WEBrick server returns ECONNRESET/EPIPE before sending Unauthorized response to client?
       raise if retry_times > 5
       retry_times += 1
+      sleep 1
       retry 
     end
   end
