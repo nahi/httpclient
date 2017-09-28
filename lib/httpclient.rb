8ca558aefdc3ad29b4e3a680fa48f736bbdd460d
Hiroshi Nakamura <nahi@ruby-lang.org>
2017-09-28 10:12:39 -0700

    raise the cause of KeepAliveDisconnected to caller
    It's rarely useful when a caller receives KeepAliveDisconnected
    exception.  By this commit caller only receives KeepAliveDisconnected
    when HTTPClient receives empty header line twice.
    
    Test should come later.

--- lib/httpclient.rb
+++ lib/httpclient.rb
@@ -1136,6 +1136,8 @@ private
       Thread.current[:HTTPClient_AcquireNewConnection] = true
       begin
         yield
+      rescue KeepAliveDisconnected => e
+        raise e.cause || e
       ensure
         Thread.current[:HTTPClient_AcquireNewConnection] = false
       end
