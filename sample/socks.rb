c709305d4ad6a569fe012dcdda7f0ac1b078f98b
Yoshinari Takaoka <mumumu@mumumu.org>
2017-10-02 03:48:29 +0900

    - fixed ENV using sample

--- sample/socks.rb
+++ sample/socks.rb
@@ -6,8 +6,8 @@ clnt = HTTPClient.new('socks4://localhost:9999')
 #clnt = HTTPClient.new('socks5://localhost:9999')
 #clnt = HTTPClient.new('socks5://username:password@localhost:9999')
 
-#socks_proxy = ENV['SOCKS_PROXY']
-#clnt = HTTPClient.new(socks_proxy)
+#ENV['SOCKS_PROXY'] = 'socks5://localhost:9999'
+#clnt = HTTPClient.new
 
 target = 'http://www.example.com/'
 puts clnt.get(target).content
